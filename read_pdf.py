13f98dda413d7336dd287487be151d9fd7bbf9f9
xB0SsZz <a93095@alunos.uminho.pt>
2022-05-18 19:30:22 +0100

    getting info complete

--- read_pdf.py
+++ read_pdf.py
@@ -1,22 +1,60 @@
 
 from PIL import Image
 import fitz
-from io import BytesIO
-import pytesseract
+
+monday = (127, 165, 250, 310)
+tuesday = (265, 165, 390, 310)
+wednesday = (407, 165, 530, 310)
+thursday = (544, 165, 670, 310)
+friday = (680, 165, 800, 310)
 
 
 def main():
-    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-    pdf = fitz.open(r"pdfs\Ementa_Cantina_Jantar.pdf")
-    page = pdf.load_page(0)
-    pix = page.get_pixmap()
+    pdf_almoco = fitz.open(r"pdfs\Ementa_Cantina_Almoco.pdf")
+    pdf_jantar = fitz.open(r"pdfs\Ementa_Cantina_Jantar.pdf")
 
-    img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
-    img.save("output.jpeg", "JPEG")
-    img = Image.open("output.jpeg")
 
-    print(pytesseract.image_to_string(img))
+    for i in range(pdf_almoco.page_count):
+        page = pdf_almoco.load_page(i)
+        pix = page.get_pixmap()
+        img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
+        monday_img = img.crop(monday)
+        tuesday_img = img.crop(tuesday)
+        wednesday_img = img.crop(wednesday)
+        thursday_img = img.crop(thursday)
+        friday_img = img.crop(friday)
+        monday_img.save("almocos\week{}\monday.jpeg".format(i + 1), "JPEG")
+        tuesday_img.save("almocos\week{}\\tuesday.jpeg".format(i + 1), "JPEG")
+        wednesday_img.save("almocos\week{}\wednesday.jpeg".format(i + 1), "JPEG")
+        thursday_img.save("almocos\week{}\\thursday.jpeg".format(i + 1), "JPEG")
+        friday_img.save("almocos\week{}\\friday.jpeg".format(i + 1), "JPEG")
+
+    for i in range(pdf_jantar.page_count):
+        page = pdf_jantar.load_page(i)
+        pix = page.get_pixmap()
+        img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
+        monday_img = img.crop(monday)
+        tuesday_img = img.crop(tuesday)
+        wednesday_img = img.crop(wednesday)
+        thursday_img = img.crop(thursday)
+        friday_img = img.crop(friday)
+        monday_img.save("jantares\week{}\monday.jpeg".format(i + 1), "JPEG")
+        tuesday_img.save("jantares\week{}\\tuesday.jpeg".format(i + 1), "JPEG")
+        wednesday_img.save("jantares\week{}\wednesday.jpeg".format(i + 1), "JPEG")
+        thursday_img.save("jantares\week{}\\thursday.jpeg".format(i + 1), "JPEG")
+        friday_img.save("jantares\week{}\\friday.jpeg".format(i + 1), "JPEG")
+
+    # page = pdf.load_page(0)
+    # pix = page.get_pixmap()
+
+    # img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
+    # img.save("output.jpeg", "JPEG")
+    # img = Image.open("output.jpeg")
+    # img_croped = img.crop(monday)
+    # img_croped.save("output2.jpeg")
+
     
+
     
 if __name__ == '__main__':
     main()
